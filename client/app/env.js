8bd3001582b0d89aa426d27c50f370c657975d87
Kaden <kjrigsbee@gmail.com>
2023-12-20 14:37:47 -0700

    we hav started

--- client/app/env.js
+++ client/app/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'dev-3cnvpw6kxrvlfdus.us.auth0.com'
+export const audience = 'https://meGusta/'
+export const clientId = 'zZxq0QmBun6pOIIKIv6YmWKy9BUgafea'
